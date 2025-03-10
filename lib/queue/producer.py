13d37a839ae9841816c5fc760018b1156c431bce
evgeniynikolaevich <zplacesound@gmail.com>
2025-03-10 06:57:55 +0300

    worker files and changes for consumer\producer

---
+++ lib/queue/producer.py
@@ -0,0 +1,21 @@
+
+
+from lib.queue.base import RabbitMQ
+
+
+class NotificationProducer(RabbitMQ):
+    """ отправляем сообщения в очередь"""
+
+    def send_notification(self, notification_type: str, notification_data: dict) -> None:
+        """ уведомления в соответствующую очередь по типу"""
+        queue_name = f'{notification_type}_notifications'
+        try:
+            self.channel.basic_publish(exchange='',
+            routing_key=queue_name, body=json.dumps(notification_data),
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # сохраним на диск локально
+            )
+            )
+        except Exception as e:
+            logger.error("Error sending notification: %s", e)
+            raise   
